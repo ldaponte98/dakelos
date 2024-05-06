0ebb1de423c841e6478a540e1c926d819886c2a5
ldaponte981 <ldaponte98@gmail.com>
2024-05-06 16:47:10 -0500

    Ajuste en redirect de sesion

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
  */
 Route::get('/', function () {
     return view('sitio.login');
-});
+})->name('/');
 Route::get('/test', function () {
     return view('app.test');
 });
