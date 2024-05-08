1a974bc07cc5757e3916fe5d7337e8ceba80225f
ldaponte981 <ldaponte98@gmail.com>
2024-05-08 10:32:19 -0500

    Nueva version de css general

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -79,7 +79,7 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.0/jquery.validate.min.js"></script>
     <script src="{{ asset('blockUI.js') }}"></script>
     <script src="{{ asset('js-general.js') }}"></script>
-    <link rel="stylesheet" type="text/css" href="{{ asset('css-general.css') }}?v=2">
+    <link rel="stylesheet" type="text/css" href="{{ asset('css-general.css') }}?v=3">
     <style>
         .btn-primary{
             background-color: {{ $licencia->color_botones }};
