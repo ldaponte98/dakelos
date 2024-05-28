ebdfbc238852d3f021fb969b503791a620d3f61c
Kelvin-orcasita <klvorcasita@gmail.com>
2024-05-27 22:04:08 -0500

    se quita imagen de canales de servicio

--- resources/views/factura/canales_servicio.blade.php
+++ resources/views/factura/canales_servicio.blade.php
@@ -62,7 +62,7 @@
             <div class="card-header">
                 <strong class="card-title pl-2">Canales de servicio</strong>
             </div>
-            <div class="card-body">
+            <div class="card-body"> 
                 <ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
                     @php $cont = 0; @endphp
                     @foreach ($canales as $item)
